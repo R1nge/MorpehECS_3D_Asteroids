0329d60c01f53969a00be49f4f9e22d260a75482
R1nge <r1ngegamedev@gmail.com>
2024-01-24 20:13:50 +0300

    First ECS system that does nothing

---
+++ Assets/_Assets/Scripts/Ecs/MovementComponent.cs
@@ -0,0 +1,14 @@
+﻿using Scellecs.Morpeh;
+using Unity.IL2CPP.CompilerServices;
+
+namespace _Assets.Scripts.Ecs
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [System.Serializable]
+    public struct MovementComponent : IComponent
+    {
+        public float speed;
+    }
+}
---
+++ Assets/_Assets/Scripts/Ecs/MovementSystem.cs
@@ -0,0 +1,30 @@
+﻿using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace _Assets.Scripts.Ecs
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(MovementSystem))]
+    public class MovementSystem : UpdateSystem
+    {
+        private Filter _movementFilter;
+
+        public override void OnAwake()
+        {
+            _movementFilter = World.Filter.With<MovementComponent>().Build();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            foreach (var entity in _movementFilter)
+            {
+                ref var movementComponent = ref entity.GetComponent<MovementComponent>();
+                Debug.Log(movementComponent.speed);
+            }
+        }
+    }
+}
