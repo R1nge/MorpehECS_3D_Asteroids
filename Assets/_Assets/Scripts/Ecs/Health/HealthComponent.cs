ca5eec7686954196d4983303e7d3dcd78e9b9b6c
R1nge <r1ngegamedev@gmail.com>
2024-01-25 16:26:24 +0300

    Destroy GO if health <= 0

--- Assets/_Assets/Scripts/Ecs/Health/HealthComponent.cs
+++ Assets/_Assets/Scripts/Ecs/Health/HealthComponent.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Scellecs.Morpeh;
 using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+using Object = UnityEngine.Object;
 
 namespace _Assets.Scripts.Ecs.Health
 {
@@ -8,8 +10,15 @@ namespace _Assets.Scripts.Ecs.Health
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
     [Serializable]
-    public struct HealthComponent : IComponent
+    public struct HealthComponent : IComponent, IDisposable
     {
         public int health;
+        public GameObject gameObject;
+
+        public void Dispose()
+        {
+            Debug.Log("Dispose");
+            Object.Destroy(gameObject);
+        }
     }
 }
---
+++ Assets/_Assets/Scripts/Ecs/Health/HealthSystem.cs
@@ -0,0 +1,33 @@
+﻿using Scellecs.Morpeh;
+using Scellecs.Morpeh.Systems;
+using Unity.IL2CPP.CompilerServices;
+using UnityEngine;
+
+namespace _Assets.Scripts.Ecs.Health
+{
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [CreateAssetMenu(menuName = "ECS/Systems/" + nameof(HealthSystem))]
+    public class HealthSystem : UpdateSystem
+    {
+        private Filter _filter;
+
+        public override void OnAwake()
+        {
+            _filter = World.Filter.With<HealthComponent>().Build();
+            World.GetStash<HealthComponent>().AsDisposable();
+        }
+
+        public override void OnUpdate(float deltaTime)
+        {
+            foreach (var entity in _filter)
+            {
+                if (entity.GetComponent<HealthComponent>().health <= 0)
+                {
+                    entity.Dispose();
+                }
+            }
+        }
+    }
+}
