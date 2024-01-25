6d36d28aaca061891b8ad125c2ebab832f8dcbfb
R1nge <r1ngegamedev@gmail.com>
2024-01-25 21:58:07 +0300

    Forgot to make asteroids component serializable

--- Assets/_Assets/Scripts/Ecs/Asteroid/AsteroidComponent.cs
+++ Assets/_Assets/Scripts/Ecs/Asteroid/AsteroidComponent.cs
@@ -1,7 +1,12 @@
 ﻿using Scellecs.Morpeh;
+using Unity.IL2CPP.CompilerServices;
 
 namespace _Assets.Scripts.Ecs.Asteroid
 {
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [System.Serializable]
     public struct AsteroidComponent : IComponent
     {
         public int points;
--- Assets/_Assets/Scripts/Ecs/Player/PlayerComponent.cs
+++ Assets/_Assets/Scripts/Ecs/Player/PlayerComponent.cs
@@ -1,7 +1,12 @@
 ﻿using Scellecs.Morpeh;
+using Unity.IL2CPP.CompilerServices;
 
 namespace _Assets.Scripts.Ecs.Player
 {
+    [Il2CppSetOption(Option.NullChecks, false)]
+    [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
+    [Il2CppSetOption(Option.DivideByZeroChecks, false)]
+    [System.Serializable]
     public struct PlayerComponent : IComponent
     {
         
